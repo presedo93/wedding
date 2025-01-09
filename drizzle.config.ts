d74a5cbda2ec366f78ce00f6448416368b2d5d88
Rene Presedo <presedo93@gmail.com>
2025-01-09 18:58:14 +0100

    feat: prepare app for production environment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,21 @@
+import fs from "fs";
 import { defineConfig } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is required");
+}
+
+let url = process.env.DATABASE_URL;
+
+if (process.env.NODE_ENV === "production") {
+  const path = process.env.DB_PASS_FILE || "/run/secrets/db-pass";
+
+  try {
+    const secret = fs.readFileSync(path, "utf8").trim();
+    url = url.replace("s3cr3t", secret);
+  } catch (error) {
+    throw new Error("Missing DB_PASS_FILE secret");
+  }
 }
 
 export default defineConfig({
@@ -10,6 +24,6 @@ export default defineConfig({
   dialect: "postgresql",
   casing: "snake_case",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url,
   },
 });
